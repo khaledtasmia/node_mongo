9026670746e847c328670c6e5f6420fead505bfc
khaledtasmia <khaled.tasmia33@gmail.com>
2021-04-05 15:45:56 +0600

    node practice

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
-const rootCall = (req, res) => res.send('I know how to open node...Yeee!');
+app.use(cors());
+app.use(express.json());
 
-app.get('/', rootCall)
+const users = ["Asad", 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana'];
+
+app.get('/', (req, res) => {
+    const fruit = {
+        product: 'ada',
+        price: 200
+    }
+    res.send(fruit);
+})
+
+app.get('/users/:id', (req,res) => {
+    const userId = req.params.id;
+    const name = users[userId];
+    res.send({userId, name});
+})
+
+//post
+app.post('/addUser', (req,res) => {
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
 
 app.listen(4200, () => console.log('Listening to port 4200'))
